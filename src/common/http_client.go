1de9b2f28a0ae279d8000be144a80fe5651f5d99
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2018-05-01 09:45:50 +0300

    Add get json method to the http client

--- src/common/http_client.go
+++ src/common/http_client.go
@@ -3,6 +3,7 @@ package common
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"reflect"
@@ -20,18 +21,41 @@ type HTTPClient struct {
 
 // PostJSON makes POST request with JSON content type.
 func (c *HTTPClient) PostJSON(url string, body interface{}, headers map[string]string, out interface{}) error {
-	var b []byte
-	var err error
-	// Marshal the body only if it is not []byte.
-	if reflect.TypeOf(body).Kind() != byteSliceKind {
-		b, err = json.Marshal(body)
-		if err != nil {
-			return err
+	return c.doRequest(url, http.MethodPost, headers, body, out)
+}
+
+// GetJSON makes GET request with JSON content type.
+func (c *HTTPClient) GetJSON(url string, headers map[string]string, out interface{}) error {
+	return c.doRequest(url, http.MethodGet, headers, nil, out)
+}
+
+func (c *HTTPClient) doRequest(url, method string, headers map[string]string, body, out interface{}) error {
+	var bodyReader io.Reader
+	if body != nil {
+		var b []byte
+		var err error
+		// Marshal the body only if it is not []byte.
+		if reflect.TypeOf(body).Kind() != byteSliceKind {
+			b, err = json.Marshal(body)
+			if err != nil {
+				return err
+			}
 		}
+
+		bodyReader = bytes.NewBuffer(b)
 	}
 
-	bReader := bytes.NewBuffer(b)
-	res, err := http.Post(url, cconstants.ContentTypeApplicationJSON, bReader)
+	req, err := http.NewRequest(method, url, bodyReader)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", cconstants.ContentTypeApplicationJSON)
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
